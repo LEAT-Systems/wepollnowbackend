fb4e3e2828fb25c3128c6f11a0265b8895f1ed1d
Stanley Egbe <57241303+Stan081@users.noreply.github.com>
2022-10-27 22:09:06 +0100

    Update settings.py

--- wepollnow/settings.py
+++ wepollnow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%vk&1)!m@4hcaw4mzcy#@rpx1bwfy&jv1xmbki+frpt!eooaj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
