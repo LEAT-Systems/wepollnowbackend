35c98ad75722afdef9b7e368c4189ed9e59c6484
Stanley Egbe <stan@MacBook-Pro.local>
2022-11-01 00:16:40 +0100

    Production ready

--- voters/models.py
+++ voters/models.py
@@ -1,24 +1,36 @@
+from audioop import maxpp
 from django.db import models
 from utilities.models import *
+import uuid
 
 # Create your models here.
-class Voter(models.Model):
+
+class Device(models.Model):
+    device_ref = models.UUIDField(primary_key=True, default=uuid.uuid5, editable=False)
     contact = models.CharField(max_length=11)
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25, blank=True)
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.contact
+
+class Voter(models.Model):
+    device_id = models.ForeignKey(Device, blank=False, null=False, on_delete=models.CASCADE)
     email = models.CharField(max_length=30)
     valid_voters_card = models.BooleanField(default="False")
     residential_status = models.BooleanField(default="True")
+    state_of_origin = models.IntegerField()
     resident_state = models.ForeignKey(States,blank=True, null=True, on_delete=models.CASCADE)
     resident_lga = models.ForeignKey(Lga, blank=True, null=True, on_delete=models.CASCADE)
-
+    age_range = models.IntegerField()
+    marital_status = models.IntegerField()
+    employment_status = models.IntegerField()
 
     def __str__(self):
         return self.contact
 
 
 class Vote(models.Model):
-    contact = models.CharField(max_length=11)
+    voter = models.ForeignKey(Voter, blank=False, null=False, on_delete=models.CASCADE)
     poll_id = models.CharField(max_length=15)
     voted_at = models.DateTimeField()
 
