6fa052cc9c7ac3aa69ec814677f593ee2391e602
Stanley <egbestanley84@yahoo.com>
2022-11-08 06:46:59 +0100

    refactor to fix db issues

--- voters/models.py
+++ voters/models.py
@@ -4,26 +4,24 @@ from utilities.models import *
 import uuid
 
 # Create your models here.
-
-class Device(models.Model):
-    device_ref = models.UUIDField(primary_key=True, default=uuid.uuid5, editable=False)
-    contact = models.CharField(max_length=11)
-    created_at = models.DateTimeField()
-
-    def __str__(self):
-        return self.contact
-
 class Voter(models.Model):
-    device_id = models.ForeignKey(Device, blank=False, null=False, on_delete=models.CASCADE)
-    email = models.CharField(max_length=30)
+    device_id = models.UUIDField(primary_key=True, default=uuid.uuid5, editable=False)
+    phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
     valid_voters_card = models.BooleanField(default="False")
     residential_status = models.BooleanField(default="True")
-    state_of_origin = models.IntegerField()
+    state_of_origin = models.ForeignKey(States,blank=True, null=True, on_delete=models.CASCADE)
     resident_state = models.ForeignKey(States,blank=True, null=True, on_delete=models.CASCADE)
     resident_lga = models.ForeignKey(Lga, blank=True, null=True, on_delete=models.CASCADE)
     age_range = models.IntegerField()
+    gender = models.IntegerField()
     marital_status = models.IntegerField()
+    religion = models.IntegerField()
     employment_status = models.IntegerField()
+    accomodation_status = models.IntegerField()
+    first_time_voter = models.BooleanField(default="False")
+    diaspora_voter = models.BooleanField(default="False")
+
 
     def __str__(self):
         return self.contact
