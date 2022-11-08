ccf6290d9cc91620435b7eda19570328ef0ca853
nalu <naluprosper54@gmail.com>
2022-11-08 12:06:45 +0100

    refactored

--- user/models.py
+++ user/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=20)
     name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=23)
+    phone_number = models.CharField(max_length=23, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Role
-from phonenumber_field.serializerfields import PhoneNumberField
 
 
 class RegisterSerializer(serializers.Serializer):
@@ -8,7 +7,7 @@ class RegisterSerializer(serializers.Serializer):
     password = serializers.CharField()
     name = serializers.CharField()
     username = serializers.CharField()
-    phone_number = PhoneNumberField(region="NG")
+    phone_number = serializers.CharField()
     
 
 
