006cc2b3c4ffc846576f7daf34f6baf9c117c887
Stanley <egbestanley84@yahoo.com>
2023-01-10 08:03:55 +0100

    db adjustments

--- wepollnow/settings.py
+++ wepollnow/settings.py
@@ -123,8 +123,12 @@ WSGI_APPLICATION = 'wepollnow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wepollnow_db',
+        'USER': 'stan',
+        'PASSWORD': '_StanLea_01',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
